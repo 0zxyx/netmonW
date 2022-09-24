1adc4fbe16fc1614d680a0d28f3f5892563fec6c
0zxyx <67796396+0zxyx@users.noreply.github.com>
2022-09-24 02:28:57 -0400

    add some fixes

--- netm.ps1
+++ netm.ps1
@@ -1,28 +1,34 @@
 while($true)
 {
- 
-$a = Get-NetAdapterStatistics
-$r1 = $a.ReceivedBytes
-$s1 = $a.SentBytes
+
+$r1 = (wmic path Win32_PerfRawData_Tcpip_NetworkInterface get BytesReceivedPersec) -replace "[^0-9]" , ''
+$s1 = (wmic path Win32_PerfRawData_Tcpip_NetworkInterface get BytesSentPersec) -replace "[^0-9]" , ''
 sleep 1
-$a = Get-NetAdapterStatistics
-$r2 = $a.ReceivedBytes
-$s2 = $a.SentBytes
+$r2 = (wmic path Win32_PerfRawData_Tcpip_NetworkInterface get BytesReceivedPersec) -replace "[^0-9]" , ''
+$s2 = (wmic path Win32_PerfRawData_Tcpip_NetworkInterface get BytesSentPersec) -replace "[^0-9]" , ''
+
+
+$rt = [math]::Round((("$r2" - "$r1") /1024),2) 
+$st = [math]::Round((("$s2" - "$s1") /1024),2)
+
+$rtm = [math]::Round((("$rt") /1024),2)
+$stm = [math]::Round((("$st") /1024),2)
+
+
+$trm = [math]::Round(("$r1" /1024 /1024),2)
+$tsm = [math]::Round(("$s1" /1024 /1024),2)
+
+$trg = [math]::Round(("$r1" /1024 /1024 /1024),2)
+$tsg = [math]::Round(("$s1" /1024 /1024 /1024),2)
+
+
  
-$rt = ($r2 - $r1) /1024
-$st = ($s2 - $s1) /1024
+	if (($rt -lt 1024) -and ($st -lt 1024))
+{ if (($trm -gt 1024) -and ($tsm -gt 1024)) {write-progress "$rt KB/s Down $st KB/s Up Received/Sent: $trg GB/$tsg GB"} elseif ($trm -gt 1024) {write-progress "$rt KB/s Down $st KB/s Up Received/Sent: $trg GB/$trm MB"}  elseif ($tsm -gt 1024) {write-progress "$rt KB/s Down $st KB/s Up Received/Sent: $trm MB/$tsg GB"} else {write-progress "$rt KB/s Down $st KB/s Up Received/Sent: $trm MB/$tsm MB"} }
+
+	else
  
-$totr = $r1 /1024 /1024
-$tots = $s1 /1024 /1024
- 
-$res1 = [math]::Round($totr,2)
-$res2 = [math]::Round($tots,2)
-$sf = [math]::Round($st,2)
- 
-        if ($rt -lt 1024)
-{$rtf = [math]::Round($rt,2); write-progress "$rtf KB/s Down $sf KB/s Up Received/Sent: $res1 MB/$res2 MB"}
-else
- 
-{$f = $rt /1024;$rtf = [math]::Round($f,2);write-progress "$rtf MB/s Down $sf KB/s Up Received/Sent:  $res1 MB/$res2 MB"}
+{ if (($trm -gt 1024) -and ($tsm -gt 1024)) {write-progress "$rtm MB/s Down $stm MB/s Up Received/Sent: $trg GB/$tsg GB"} elseif ($trm -gt 1024) {write-progress "$rtm MB/s Down $stm MB/s Up Received/Sent: $trg GB/$tsm MB"}  elseif ($tsm -gt 1024) {write-progress "$rtm MB/s Down $stm MB/s Up Received/Sent: $trm MB/$tsg GB"} else {write-progress "$rtm MB/s Down $rts MB/s Up Received/Sent: $trm MB/$tsm MB"} }
+
  
 }
